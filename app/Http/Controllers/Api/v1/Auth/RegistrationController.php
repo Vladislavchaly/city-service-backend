c6368b101d62533ba5d92730fe87c2d8142f2196
chaly95 <chaly95@gmail.com>
2022-07-26 10:24:49 +0300

    Update README.md and apidoc.example

--- app/Http/Controllers/Api/v1/Auth/RegistrationController.php
+++ app/Http/Controllers/Api/v1/Auth/RegistrationController.php
@@ -1,33 +1,25 @@
 <?php
-//
-//namespace App\Http\Controllers\Api\v1\Auth;
-//
-//use App\Http\Requests\Api\v1\Auth\RegistrationRequest;
-//use App\Notifications\Auth\SuccessRegistration;
-//use App\Repositories\Eloquent\UsersRepository;
-//
-//class RegistrationController extends \App\Http\Controllers\Api\ResponseApiController
-//{
-//    public function __invoke(RegistrationRequest $request)
-//    {
-//        // Create new user
-//        $user = UsersRepository::createUser($request->all());
-//
-//        // Send contact to agile crm
-//        \App\Jobs\ZapierJob::dispatch('signUp', [
-//            [
-//                'email' => $user->email,
-//                'Registrant Type' => $user->role->title,
-//            ]
-//        ]);
-//
-//        // Send success registration email
-//        $user->notify(
-//            (new SuccessRegistration($request->get('password')))->locale(app()->getLocale())
-//        );
-//
-//        return response()->success(
-//            \App\Http\Resources\Api\v1\Auth\RegistrationResource::make($user)
-//        );
-//    }
-//}
+
+namespace App\Http\Controllers\Api\v1\Auth;
+
+use App\Http\Requests\Api\v1\Auth\RegistrationRequest;
+use App\Notifications\Auth\SuccessRegistration;
+use App\Repositories\Eloquent\UsersRepository;
+
+class RegistrationController extends \App\Http\Controllers\Api\ResponseApiController
+{
+    public function __invoke(RegistrationRequest $request)
+    {
+        // Create new user
+        $user = UsersRepository::createUser($request->all());
+
+        // Send success registration email
+        $user->notify(
+            (new SuccessRegistration($request->get('password')))->locale(app()->getLocale())
+        );
+
+        return response()->success(
+            \App\Http\Resources\Api\v1\Auth\RegistrationResource::make($user)
+        );
+    }
+}
--- app/Http/Controllers/Api/v1/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Api/v1/Auth/ResetPasswordController.php
@@ -1,21 +1,21 @@
 <?php
-//
-//namespace App\Http\Controllers\Api\v1\Auth;
-//
-//use App\Http\Requests\Api\v1\Auth\ResetPasswordRequest;
-//use App\Notifications\Auth\ResetPassword;
-//use App\Repositories\Eloquent\UsersRepository;
-//
-//class ResetPasswordController extends \App\Http\Controllers\Api\ResponseApiController
-//{
-//    public function __invoke(ResetPasswordRequest $request)
-//    {
-//        $user = UsersRepository::resetPassword($request->all());
-//
-//        if ($user) {
-//            $user->notify((new ResetPassword($user->unhash_password))->locale(app()->getLocale()));
-//        } else {
-//            return response()->error(trans('auth.check_false_email'));
-//        }
-//    }
-//}
+
+namespace App\Http\Controllers\Api\v1\Auth;
+
+use App\Http\Requests\Api\v1\Auth\ResetPasswordRequest;
+use App\Notifications\Auth\ResetPassword;
+use App\Repositories\Eloquent\UsersRepository;
+
+class ResetPasswordController extends \App\Http\Controllers\Api\ResponseApiController
+{
+    public function __invoke(ResetPasswordRequest $request)
+    {
+        $user = UsersRepository::resetPassword($request->all());
+
+        if ($user) {
+            $user->notify((new ResetPassword($user->unhash_password))->locale(app()->getLocale()));
+        } else {
+            return response()->error(trans('auth.check_false_email'));
+        }
+    }
+}
