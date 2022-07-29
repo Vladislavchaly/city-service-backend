96628605fff349a77691959cab0bd7428aa0f357
chaly95 <chaly95@gmail.com>
2022-07-29 21:59:19 +0300

    add Registration Controller

--- app/Http/Controllers/Api/v1/Auth/RegistrationController.php
+++ app/Http/Controllers/Api/v1/Auth/RegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\v1\Auth;
 use App\Http\Requests\RegistrationRequest;
 use App\Interfaces\Repositories\UsersRepositoryInterface;
 use Symfony\Component\HttpFoundation\Response;
+
 //TODO Add email confirmation
 //TODO Add referral email
 //TODO move create token functional to extra class
@@ -15,9 +16,11 @@ class RegistrationController
         $request->validated();
         $user = $usersRepository->create($request->all());
 
-        return response()->json([
-            'token' => 'Bearer ' . $user->createToken('Laravel Password Grant Client')->accessToken,
-        ], Response::HTTP_OK
+        return response()->json(
+            [
+                'token' => 'Bearer ' . $user->createToken('Laravel Password Grant Client')->accessToken,
+            ],
+            Response::HTTP_OK
         );
     }
 }
